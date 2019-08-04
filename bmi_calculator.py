0080df0e43cb384d018f019b8c91ba13e8fb2e9f
Paven Sharma <pavensharma90@gmail.com>
2019-08-04 21:52:34 +0800

    added formula for bmi calculation

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,1 +1,9 @@
-print("hello world")
+weight = input("Insert your weight (in KG)")
+height = input("insert your height (M)")
+
+weight = float(weight)
+height = float(height)
+
+bmi = weight / (height**2)
+
+print(bmi)
